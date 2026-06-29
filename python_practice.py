b600cfe31fb50082efc07a5fbac465bf6bd131d4
PRAGYA SISODIYA <pragyasisodiya23092006@gmail.com>
2026-06-29 18:49:06 +0530

    Reversing the list

--- python_practice.py
+++ python_practice.py
@@ -656,26 +656,104 @@
 # print("My run has run for:",count,"times")
 
 #ASSIGNMENT PROBLEM 1
+'''
+Practice Question
+The Beatles were one of the most popular music groups of the 1960s, and the best-selling band in history. Some people consider them to be the most influential act of the rock era. Indeed, they were included in Time magazine's compilation of the 20th Century's 100 most influential people.
+The band underwent many line-up changes, culminating in 1962 with the line-up of John Lennon, Paul McCartney, George Harrison, and Richard Starkey (better known as Ringo Starr).
+Write a program that reflects these changes and lets you practice with the concept of lists. Your task is to:
+step 1: create an empty list named beatles;step 2: use the append() method to add the following members of the band to the list: John Lennon, Paul McCartney, and George Harrison;step 3: use the for loop and the append() method to prompt the user to add the following members of the band to the list: Stu Sutcliffe, and Pete Best;step 4: use the del instruction to remove Stu Sutcliffe and Pete Best from the list;step 5: use the insert() method to add Ringo Starr to the beginning of the list.
+'''
+# beatles=[]
+# beatles.append("John Lennon")
+# beatles.append("Paul McCartney")
+# beatles.append("George Harrison")
+# print(beatles)
 
-beatles=[]
-beatles.append("John Lennon")
-beatles.append("Paul McCartney")
-beatles.append("George Harrison")
-print(beatles)
+# user=["Stu Sutcliffe","Pete Best"]
+# i=0
+# for i in range(2):
+#     beatles.append(user[i])
 
-user=["Stu Sutcliffe","Pete Best"]
-i=0
-for i in range(2):
-    beatles.append(user[i])
+# print(beatles)
 
-print(beatles)
+# del beatles[len(beatles)-1]
+# del beatles[len(beatles)-1]
+# print(beatles)
 
-del beatles[len(beatles)-1]
-del beatles[len(beatles)-1]
-print(beatles)
+# beatles.insert(0,"Ringo Starr")
+# print(beatles)
 
-beatles.insert(0,"Ringo Starr")
-print(beatles)
+#26/06/2026   Friday
+# my_list=[8,10,6,2,4]
+# swapped=True     #It's a little fake,we need it to enter the while loop
+# count=0
+# while swapped:
+#     swapped=False  #no swaps so far
+#     for i in range(len(my_list)-1):
+#         count+=1
+#         if my_list[i] > my_list[i+1]:
+#             swapped=True  #a swap occured
+#             my_list[i] , my_list[i+1] = my_list[i+1] , my_list[i]
+# print(my_list)
+# print("Loops are running for:",count,"times")
+
+'''
+'''
+
+
+# my_list=[1,2,3,4,5]  #already sorted (best case)
+# swapped=0
+# print(my_list)
+# count=0
+# for index1 in range(len(my_list)-1):
+#     # for index in range(len(my_list)-1):
+#     for index in range(len(my_list)-1-index1):   #efficient case
+#         count+=1
+#         if my_list[index] > my_list[index+1]:
+            
+#             my_list[index] , my_list[index+1] = my_list[index+1] , my_list[index]
+#             swapped=1
+#         if swapped==0:
+#             break
+
+# print(my_list)
+# print("My run has run for:",count,"times")  #4 times
+
+# my_list=[8,10,6,2,4]
+# print(my_list)
+# my_list.sort()
+# print(my_list)
+
+# lst=[5,3,1,2,4]
+# print(lst)
+# lst.reverse()
+# print(lst)
+
+'''
+logic
+length=x
+[a,b,c,d,e,f,g]
+[g,f,e,d,c,b,a]
+
+0- -1
+1- -2 
+-1*(index+1
+)'''
+
+
+lst=[5,3,1,2,4]
+print(len(lst)//2)
+print(lst)
+for i in range(len(lst)//2):
+    # lst[i],lst[(-1)*(i+1)]=lst[(-1)*(i+1)],lst[i]
+    lst[i],lst[len(lst)-(i+1)]=lst[len(lst)-(i+1)],lst[i]
+
+    
+print(lst)
+
+        
+
+
 
 
 
